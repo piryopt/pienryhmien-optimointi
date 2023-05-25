cb9084a9377e5299c5b0ebaafaa2fc128398d2da
Matias Siro <matias.siro@outlook.com>
2023-05-25 11:40:51 +0300

    muutettu Dockerfileä

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,13 @@
-FROM alpine:3.14
+# Use the official Python image as the base image
+FROM python:3.9
 
 WORKDIR /usr/src/app
 
 COPY ./src .
 
-RUN apk add python3
-RUN apk add py3-pip
-RUN pip3 install -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
 
 
 CMD ["python3", "-m", "flask", "run", "--host=0.0.0.0"]
--- src/app.py
+++ src/app.py
@@ -1,5 +1,11 @@
 from flask import Flask, render_template
+from flask import redirect, render_template, request, session
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+
+#db = SQLAlchemy(app)
+#app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 
 @app.route("/")
 def hello_world() -> str:
@@ -7,4 +13,10 @@ def hello_world() -> str:
     Returns the rendered skeleton template
     """
     return render_template('index.html')
+
+def db_connection_test():
+    """
+    Tests db-connection. 
+    """
+
 
