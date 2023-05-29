7494a591c2948a3e1e668b37b6321cd910b686c9
Matias Siro <matias.siro@outlook.com>
2023-05-29 10:47:40 +0300

    Database configurations

--- src/app.py
+++ src/app.py
@@ -2,28 +2,37 @@ from flask import Flask, render_template
 from flask import redirect, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 from db_urls import DatabaseURL
-import routes
+
+#import routes
 
 app = Flask(__name__)
-app.debug = True
-
-db = SQLAlchemy(app)
-
 
 database_url = DatabaseURL()
 
-string_template = "postgresql://{username}:{password}@{host}:{port}/{database}"
-DATABASE_URL = string_template.format(database_url.get_user_production, database_url.get_production_password, database_url.get_host, database_url.get_port, database_url.get_database_production)
+username = database_url.get_user_production()
+password = database_url.get_production_password()
+port = database_url.get_port()
+host = database_url.get_host()
+database = database_url.get_database_production()
 
-#DATABASE_URL_TEST = database_url.get_url("test")
+
+DATABASE_URL = "postgresql://" + username + ":" + password + "@" + host + ":" + str(port) + "/" + database
 
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+db = SQLAlchemy(app)
+
+app.debug = True
 
 # TODO: entäs testikanta?
 
-import routes
+
 
 '''def db_connection_test():
     """
     Tests db-connection. 
     """'''
+
+
+
+if __name__ == '__main__':
+    app.run()
