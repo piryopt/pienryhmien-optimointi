550a830402c6dac054b2d248831aec598faa9892
Heli Parviainen <heli.s.parviainen@helsinki.fi>
2023-06-06 13:10:31 +0300

    Quick fix results display

--- src/routes.py
+++ src/routes.py
@@ -57,5 +57,5 @@ def excel():
     sort.run()
     output_data = sort.get_data()
 
-    return render_template("results.html", results1 = output_data.selections, happiness_data = output_data.happiness_data,
+    return render_template("results.html", results = output_data.selections, happiness_data = output_data.happiness_data,
                            time = output_data.time, happiness = output_data.happiness)
