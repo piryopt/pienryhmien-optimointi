ec28bc923b23b271f97a3617267114cf826bff06
Mikko Anttonen <mikkohj.anttonen@gmail.com>
2023-06-09 00:36:41 +0300

    Add placeholder route & template for group selections

--- src/routes.py
+++ src/routes.py
@@ -57,3 +57,7 @@ def excel():
 
     return render_template("results.html", results = output_data.selections, happiness_data = output_data.happiness_data,
                            time = output_data.time, happiness = output_data.happiness)
+
+@app.route("/groups")
+def groups():
+    return render_template("groups.html")
