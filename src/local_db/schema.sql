f6b7f566a4f6175845393d122675324464b80746
masiro918 <matias.siro@outlook.com>
2023-06-09 12:48:34 +0300

    Add configures to local db

---
+++ src/local_db/schema.sql
@@ -0,0 +1,55 @@
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	firstname TEXT,
+ 	lastname TEXT,
+ 	student_number TEXT, -- or int or not at all
+	isteacher BOOLEAN
+);
+
+CREATE TABLE courses (
+	id SERIAL PRIMARY KEY,
+	coursename TEXT UNIQUE,
+	teacher_id INTEGER REFERENCES users -- maybe not needed
+);
+
+CREATE TABLE surveys ( -- yksittäinen kysely
+	id SERIAL PRIMARY KEY,
+	groupname TEXT,
+	course_id INTEGER REFERENCES courses,
+	min_choices INTEGER
+);
+
+CREATE TABLE choices ( -- yksittäinen päiväkoti, pienryhmä
+	id SERIAL PRIMARY KEY,
+	course_id INTEGER REFERENCES courses,
+	survey_id INTEGER REFERENCES surveys,
+	name TEXT,
+	max_spaces INTEGER,
+	current_spaces INTEGER,
+	info1 TEXT,
+	info2 TEXT
+);
+
+CREATE TABLE rankings (
+	id SERIAL PRIMARY KEY,
+	user_id INTEGER REFERENCES users,
+	survey_id INTEGER REFERENCES surveys,
+	choice_id INTEGER REFERENCES choices,
+	ranking INTEGER
+);
+
+CREATE TABLE participants (
+	id SERIAL PRIMARY KEY,
+	course_id INTEGER REFERENCES courses,
+	user_id INTEGER REFERENCES users,
+	privileged BOOLEAN
+);
+
+CREATE TABLE final_group ( -- lopullinen sijoitus
+	id SERIAL PRIMARY KEY,
+	course_id INTEGER REFERENCES courses,
+	user_id INTEGER REFERENCES users,
+	survey_id INTEGER REFERENCES surveys,
+	choice_id INTEGER REFERENCES choices
+);
+
--- src/run_flask_app.sh
+++ src/run_flask_app.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export DATABASE_URL="postgresql://localhost:/piryopt?user=postgres&password=local_test"
+
 pip3 install --no-cache-dir -r requirements.txt
 
 python3 -m flask run
