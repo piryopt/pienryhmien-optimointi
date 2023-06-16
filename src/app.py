e272d99a1fa47c8901776704475f6e62679ee5c9
Jerry <tammi.jerry@gmail.com>
2023-06-16 13:29:29 +0300

    Poetry and tests edit

--- src/app.py
+++ src/app.py
@@ -3,9 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 from dotenv import load_dotenv
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-
+load_dotenv()
 
 app = Flask(__name__)
 
