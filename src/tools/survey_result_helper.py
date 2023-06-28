02c3266e2e38b20cdb41d879310232ff75f419b8
Jerry <tammi.jerry@gmail.com>
2023-06-28 20:06:33 +0300

    Bug fix

--- src/tools/survey_result_helper.py
+++ src/tools/survey_result_helper.py
@@ -15,6 +15,6 @@ def convert_users_students(user_rankings):
         user = user_repository.get_user_data(user_id)
         ranking = user_ranking[1].split(",")
         int_ranking = [int(i) for i in ranking]
-        student_name = user[0].firstname + " " + user[0].lastname
+        student_name = user[0].name
         students[user_id] = Student(user_id, student_name, int_ranking)
     return students
