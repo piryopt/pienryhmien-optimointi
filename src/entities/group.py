bc90b0405f0f9bffd23f3e524e7cbc743da88b55
Heli Parviainen <heli.s.parviainen@helsinki.fi>
2023-06-28 16:11:54 +0300

    Remove unnescessary hospital algo data structures

--- src/entities/group.py
+++ src/entities/group.py
@@ -5,12 +5,3 @@ class Group:
         self.participants = []
         self.prio = []
         self.size = size
-
-    def get_average_happiness(self, students_dict):
-        '''Returns the average happiness of all users of the group'''
-        sum = 0
-        for p in self.participants:
-            sum += students_dict[p].happiness
-        if sum == 0:
-            return 0
-        return sum / len(self.participants)
