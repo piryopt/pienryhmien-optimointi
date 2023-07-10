38ff81a4186a3ae7d46b79aa9cd62e884b77fe95
Jerry <tammi.jerry@gmail.com>
2023-07-10 19:07:50 +0300

    New tests

--- tests/survey_repository_test.py
+++ tests/survey_repository_test.py
@@ -18,6 +18,14 @@ class TestSurveyRepository(unittest.TestCase):
         self.app_context = self.app.app_context()
         self.app_context.push()
 
+        self.ur = ur
+        user1 = User("Not on tren Testerr", 101010101, "feelsbadman@tester.com", True)
+        user2 = User("Not on anabolic", 101010101, "anabolic@tester.com", True)
+        self.ur.register(user1)
+        self.ur.register(user2)
+        self.user_id = ur.find_by_email(user1.email)[0]
+        self.user_id2 = ur.find_by_email(user2.email)[0]
+
     def tearDown(self):
         db.drop_all()
         self.app_context.pop()
@@ -26,7 +34,7 @@ class TestSurveyRepository(unittest.TestCase):
         """
         Create new survey and test if it exists and also test if the surveyname exists
         """
-        test_survey1_id = sr.add_new_survey("Test survey 1")
+        test_survey1_id = sr.add_new_survey("Test survey 1", self.user_id)
         survey = sr.check_if_survey_exists(test_survey1_id)
         self.assertEqual(survey[0], test_survey1_id)
 
@@ -41,7 +49,7 @@ class TestSurveyRepository(unittest.TestCase):
         """
         Test that adding and finding survey choices works
         """
-        test_survey2_id = sr.add_new_survey("Test survey 2")
+        test_survey2_id = sr.add_new_survey("Test survey 2", self.user_id)
         for i in range(3):
             sr.add_new_survey_choice(test_survey2_id, "test" + str(i) + "choice", 10, "info1", "info2")
         n = len(sr.find_survey_choices(test_survey2_id))
@@ -58,17 +66,15 @@ class TestSurveyRepository(unittest.TestCase):
         """
         Test that adding and deleting user rankings works
         """
-        ur.register(User("Testosterone Tester", 1010101011, "testosterone.tester@test.com", True))
-        user_id = ur.find_by_email("testosterone.tester@test.com").id
-        test_survey3_id = sr.add_new_survey("Test survey 3")
+        test_survey3_id = sr.add_new_survey("Test survey 3", self.user_id)
         for i in range(3):
             sr.add_new_survey_choice(test_survey3_id, "test" + str(i) + "choice", 10, "info1", "info2")
-        sr.add_user_ranking(user_id, test_survey3_id, "1,2,3")
-        ranking = sr.get_user_ranking(user_id, test_survey3_id)
+        sr.add_user_ranking(self.user_id, test_survey3_id, "1,2,3")
+        ranking = sr.get_user_ranking(self.user_id, test_survey3_id)
         self.assertEqual(ranking[3], "1,2,3")
 
-        sr.delete_user_ranking(user_id, test_survey3_id)
-        ranking = sr.get_user_ranking(user_id, test_survey3_id)
+        sr.delete_user_ranking(self.user_id, test_survey3_id)
+        ranking = sr.get_user_ranking(self.user_id, test_survey3_id)
 
         self.assertEqual(ranking, False)
 
@@ -76,8 +82,8 @@ class TestSurveyRepository(unittest.TestCase):
         """
         Test that surveyname exists
         """
-        test_survey4_id = sr.add_new_survey("Test survey 4")
-        exists = sr.survey_name_exists("Test survey 4")
+        sr.add_new_survey("Test survey 4", self.user_id)
+        exists = sr.survey_name_exists("Test survey 4", self.user_id)
         self.assertEqual(True, exists)
 
     def test_survey_name_doesnt_exist(self):
@@ -85,8 +91,39 @@ class TestSurveyRepository(unittest.TestCase):
         Test that surveyname doesn't exist
         """
         test_survey5_name = "Test survey 5"
-        exists = sr.survey_name_exists(test_survey5_name)
+        exists = sr.survey_name_exists(test_survey5_name, self.user_id)
         self.assertEqual(False, exists)
+
+    def test_count_created_surveys(self):
+        """
+        Test that the number of created surveys is correct
+        """
+
+        sr.add_new_survey("Test survey 5", self.user_id2)
+        sr.add_new_survey("Test survey 6", self.user_id2)
+        count = sr.count_created_surveys(self.user_id2)
+        self.assertEqual(2, count)
+
+    def test_close_survey(self):
+        """
+        Test that closing a survey works
+        """
+        survey_id = sr.add_new_survey("Test survey 7", self.user_id)
+        sr.close_survey(survey_id, self.user_id)
+
+        closed = sr.check_if_survey_exists(survey_id).closed
+        self.assertEqual(True, closed)
+
+    def test_get_active_surveys(self):
+        active_list = sr.get_active_surveys(self.user_id2)
+        self.assertEqual(2, len(active_list))
+
+    def test_get_closed_surveys(self):
+        survey_id = sr.add_new_survey("Test survey 8", self.user_id2)
+        sr.close_survey(survey_id, self.user_id2)
+        closed_list = sr.get_closed_surveys(self.user_id2)
+        self.assertEqual(1, len(closed_list))
+
 
 if __name__ == "__main__":
     unittest.main()
