58018b4a115b02f194f5a99d2a8db65865fb9e81
Rasmus S <rasmus.salmela@gmail.com>
2023-08-01 22:53:13 +0300

    Fix weird surveys reason disappearance

--- src/routes.py
+++ src/routes.py
@@ -125,7 +125,7 @@ def surveys(survey_id):
             return render_template("closedsurvey.html", bad_survey_choices = bad_survey_choices, good_survey_choices=good_survey_choices, survey_name = survey_name)
         return render_template("survey.html", choices = survey_choices, survey_id = survey_id,
                             survey_name = survey_name, existing = existing, desc = desc,
-                            bad_survey_choices = bad_survey_choices, good_survey_choices=good_survey_choices)
+                            bad_survey_choices = bad_survey_choices, good_survey_choices=good_survey_choices, reason=reason)
         
         
 
