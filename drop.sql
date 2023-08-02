a6d8e82fed2cf1f9079297dfd1e288f4c8108a5a
Heli Parviainen <heli.s.parviainen@helsinki.fi>
2023-08-02 11:29:45 +0300

    modify drop order

--- drop.sql
+++ drop.sql
@@ -1,6 +1,6 @@
-DROP TABLE users CASCADE;
-DROP TABLE surveys CASCADE;
-DROP TABLE survey_choices CASCADE;
-DROP TABLE choice_infos CASCADE;
-DROP TABLE user_survey_rankings CASCADE;
 DROP TABLE final_group;
+DROP TABLE user_survey_rankings CASCADE;
+DROP TABLE choice_infos CASCADE;
+DROP TABLE survey_choices CASCADE;
+DROP TABLE surveys CASCADE;
+DROP TABLE users CASCADE;
