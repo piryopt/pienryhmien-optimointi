8b04c5444c4efb2bd7323b30331bd25f99af81d9
Heli Parviainen <heli.s.parviainen@helsinki.fi>
2023-08-07 10:15:37 +0300

    Update drop.sql to match database schema

--- drop.sql
+++ drop.sql
@@ -1,7 +1,7 @@
-DROP TABLE final_group;
+DROP TABLE final_group CASCADE;
 DROP TABLE user_survey_rankings CASCADE;
 DROP TABLE choice_infos CASCADE;
 DROP TABLE survey_choices CASCADE;
 DROP TABLE survey_teachers CASCADE;
-DROP TABLE surveys CASCADE;
+DROP TABLE surveys;
 DROP TABLE users CASCADE;
