46c476d7a287d1d9d7574412721eadc1d8f926ee
Viljami Latvala <viljami.latvala@helsinki.fi>
2023-08-22 10:57:37 +0300

    finalize saving search visibility preference to db

--- src/routes.py
+++ src/routes.py
@@ -187,7 +187,7 @@ def new_survey_post():
     allowed_denied_choices = data["allowedDeniedChoices"]
     allow_search_visibility = data["allowSearchVisibility"]
 
-    survey_id = survey_service.create_new_survey_manual(survey_choices, survey_name, user_id, description, minchoices, date_begin, time_begin, date_end, time_end, allowed_denied_choices)
+    survey_id = survey_service.create_new_survey_manual(survey_choices, survey_name, user_id, description, minchoices, date_begin, time_begin, date_end, time_end, allowed_denied_choices, allow_search_visibility)
     if not survey_id:
         response = {"status":"0", "msg":"Tämän niminen kysely on jo käynnissä! Sulje se tai muuta nimeaä!"}
         return jsonify(response)
