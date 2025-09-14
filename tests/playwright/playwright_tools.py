418bd6e67962f62ec0da36668f4d91b636aa0ece
Fialco <113672867+Fialco@users.noreply.github.com>
2025-09-14 17:51:14 +0300

    Add local testing through Docker
    - Add test service to docker-compose
    - Add initial testing guide
    - Move playwright to dev dependencies in pyproject.toml
    - Add check if playwright is ran through localhost or in a container

--- tests/playwright/playwright_tools.py
+++ tests/playwright/playwright_tools.py
@@ -1,8 +1,11 @@
+import os
+
 def login(page, username, password):
     """
     Helper function for logging the user in
     """
-    page.goto("http://127.0.0.1:5000/")
+    base_url = os.getenv("BASE_URL", "http://127.0.0.1:5000/")
+    page.goto(base_url)
     page.locator("#nameid").fill(username)
     page.get_by_label("Salasana (laita mitä vaan):").fill(password)
     page.get_by_role("button").click()
