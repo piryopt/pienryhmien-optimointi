915e018b76f1e92c90c6c02e7c08c82ffd94363c
Fialco <113672867+Fialco@users.noreply.github.com>
2025-09-22 10:43:03 +0300

    Try to fix CI playwright
    - Start using traces upon failure
    - Swap xpath locators to nth locators

--- tests/playwright/playwright_tools.py
+++ tests/playwright/playwright_tools.py
@@ -1,4 +1,15 @@
 import os
+import pytest
+
+@pytest.fixture(scope="function", autouse=True)
+def trace_on_failure(context, request):
+    context.tracing.start(screenshots=True, snapshots=True, sources=True)
+    yield
+    if request.node.rep_call.failed:
+        trace_path = f"trace_{request.node.name}.zip"
+        context.tracing.stop(path=trace_path)
+    else:
+        context.tracing.stop()
 
 def login(page, username, password):
     """
