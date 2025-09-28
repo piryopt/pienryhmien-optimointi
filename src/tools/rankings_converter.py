9741a3704b206a78e3b0568c45b5321cbe3fbac8
Nanna Ketola <nannailona@gmail.com>
2025-09-28 15:44:32 +0300

    Add function for converting ranking string to a list of ints

--- src/tools/rankings_converter.py
+++ src/tools/rankings_converter.py
@@ -10,6 +10,21 @@ def convert_to_list(ranking_string):
     ranking_list = ranking_string.split(",")
     return ranking_list
 
+def convert_to_int_list(ranking_string):
+    """
+    Converts a list consisting of the rankings of a survey into a list of integers. E.G ["2", "3", "5", "6", "4", "7"] -> [2, 3, 5, 6, 4, 7]
+
+    args:
+        ranking_list: The list that is converted into a list of integers. The list is retrieved from the database table user_survey_rankings
+    """
+    if ranking_string == '':
+        return []
+    ranking_list = ranking_string.split(",")
+
+    int_ranking = [int(i) for i in ranking_list]
+
+    return int_ranking
+
 def convert_to_string(ranking_list):
     """
     Converts a list consisting of the rankings of a survey into a string. E:G ["2", "3", "5", "6", "4", "7"] -> "2,3,5,6,4,7"
