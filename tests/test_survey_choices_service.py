c7dc8a69156f38b05a92216a916b09489a81c459
Nanna Ketola <nannailona@gmail.com>
2025-10-05 13:17:56 +0300

    Add pytests for survey_choices_service and modify test_survey1.json

--- tests/test_survey_choices_service.py
+++ tests/test_survey_choices_service.py
@@ -13,7 +13,6 @@ from src.tools.db_tools import clear_database
 import json
 
 
-
 @pytest.fixture(autouse=True)
 def setup_env():
     """
@@ -63,13 +62,13 @@ def setup_env():
     app_context.pop()
 
 
-
 def test_get_list_of_survey_choices_returns_false_if_no_data_found():
     """
     Inputs a nonexistent survey id to getter and checks that return is "False"
     """
     ret = scs.get_list_of_survey_choices("ITSNOTREAL")
     assert ret is False
+
 
 def test_get_list_of_survey_choices_returns_correct_number_of_choices(setup_env):
     """
@@ -78,6 +77,7 @@ def test_get_list_of_survey_choices_returns_correct_number_of_choices(setup_env)
     d = setup_env
     choices = scs.get_list_of_survey_choices(d["survey_id"])
     assert len(choices) == 2
+
 
 def test_get_list_of_survey_choices_returns_correct_number_of_spaces(setup_env):
     """
@@ -88,6 +88,7 @@ def test_get_list_of_survey_choices_returns_correct_number_of_spaces(setup_env):
     choices = scs.get_list_of_survey_choices(d["survey_id"])
     assert choices[0][3] + choices[1][3] == 14
 
+
 def test_get_list_of_survey_choices_returns_correct_choice_names(setup_env):
     """
     Tests that get_list_of_survey_choices() returns a list of choices where
@@ -96,6 +97,8 @@ def test_get_list_of_survey_choices_returns_correct_choice_names(setup_env):
     d = setup_env
     choices = scs.get_list_of_survey_choices(d["survey_id"])
     assert choices[0][2] + " " + choices[1][2] == "Esimerkkipäiväkoti 1 Esimerkkipäiväkoti 2"
+
+
 def test_get_survey_choice_returns_false_if_survey_not_found():
     """
     Tests that function get_survey_choice() returns false if no
@@ -103,6 +106,7 @@ def test_get_survey_choice_returns_false_if_survey_not_found():
     when ids should be int
     """
     assert scs.get_survey_choice("Not an id") is False
+
 
 def test_get_survey_choice_gets_correct_choice(setup_env):
     """
@@ -115,6 +119,27 @@ def test_get_survey_choice_gets_correct_choice(setup_env):
     one_choice = scs.get_survey_choice(choices[0][0])
     assert choices[0][2] == one_choice[2]
 
+
+def test_get_survey_choice_min_size(setup_env):
+    """
+    Fetches all choices with get_list_of_survey_choices() and inputs
+    the first result choice id to function get_survey_choice_min_size(),
+    then tests that the returned min_size value is correct
+    """
+    d = setup_env
+    choices = scs.get_list_of_survey_choices(d["survey_id"])
+    min_size = scs.get_survey_choice_min_size(choices[0].id)
+    assert min_size == choices[0].min_size
+
+
+def test_get_survey_choice_min_size_invalid_id():
+    """
+    Tests that function get_survey_choice_min_size returns False
+    when an invalid id is used as input
+    """
+    assert scs.get_survey_choice_min_size("Not an id") is False
+
+
 def test_get_choice_name_and_spaces_gets_correct_choice(setup_env):
     """
     Fetches all choices with get_list_of_survey_choices() and inputs
@@ -125,6 +150,7 @@ def test_get_choice_name_and_spaces_gets_correct_choice(setup_env):
     choices = scs.get_list_of_survey_choices(d["survey_id"])
     _, name, spaces = scs.get_choice_name_and_spaces(choices[0][0])
     assert choices[0][2] == name
+
 
 def test_get_choice_additional_infos_returns_correct_data(setup_env):
     """
@@ -140,6 +166,7 @@ def test_get_choice_additional_infos_returns_correct_data(setup_env):
     # info
     assert choice_infos[0][1] + " " + choice_infos[1][1] == "Keijukaistenpolku 14 00820"
 
+
 def test_count_number_of_available_spaces(setup_env):
     """
     Tests that function count_number_of_available_spaces returns the
@@ -147,7 +174,6 @@ def test_count_number_of_available_spaces(setup_env):
     """
     d = setup_env
     assert scs.count_number_of_available_spaces(d["survey_id"]) == 14
-
 
 
 def test_add_empty_survey_choice(setup_env):
@@ -161,3 +187,51 @@ def test_add_empty_survey_choice(setup_env):
     choices = scs.get_list_of_survey_choices(d["survey_id"])
     assert choices[2].name == "Tyhjä"
     assert choices[2].max_spaces == 3
+
+
+def test_check_min_equals_max(setup_env):
+    """
+    Tests that function check_min_equal_max() returns false
+    when min_size and max_size are not equal for all of the choice
+    """
+    d = setup_env
+    assert scs.check_min_equals_max(d["survey_id"]) == (False, 0)
+
+
+def test_get_survey_choice_mandatory(setup_env):
+    """
+    Tests that function survey_choice_mandatory_field() returns true when the
+    choice is mandatory
+    """
+    d = setup_env
+    choices = scs.get_list_of_survey_choices(d["survey_id"])
+    result = scs.get_survey_choice_mandatory(choices[1][0])
+    assert result is True
+
+
+def test_get_survey_choice_mandatory_returns_false_when_group_is_not_mandatory(setup_env):
+    """
+    Tests that function survey_choice_mandatory_field() returns false
+    when group is not mandatory
+    """
+    d = setup_env
+    choices = scs.get_list_of_survey_choices(d["survey_id"])
+    assert scs.get_survey_choice_mandatory(choices[0][0]) is False
+
+
+def test_check_answers_less_than_min_size(setup_env):
+    """
+    Tests that function check_answers_less_than_min_size() returns true if
+    the number of answers is less than the min size of the group with the smallest min size
+    """
+    d = setup_env
+    assert scs.check_answers_less_than_min_size(d["survey_id"], 0) is True
+
+
+def test_check_answers_less_than_min_size_returns_false_if_answers_greater_than_min_size(setup_env):
+    """
+    Tests that function check_answers_less_than_min_size() returns false if
+    the number of answers is greater than the min size of the group with the smallest min size
+    """
+    d = setup_env
+    assert scs.check_answers_less_than_min_size(d["survey_id"], 5) is False
