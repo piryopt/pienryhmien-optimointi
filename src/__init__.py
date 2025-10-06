e6e1f37c7e8e0c636637768146d2b28a40424d0f
Fialco <113672867+Fialco@users.noreply.github.com>
2025-10-06 11:50:40 +0300

    Refactor main init file and routes.py

--- src/__init__.py
+++ src/__init__.py
@@ -5,47 +5,67 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 from flask_babel import Babel
 from dotenv import load_dotenv
+from src.tools.date_converter import format_datestring
+
+csfr = CSRFProtect()
+db = SQLAlchemy()
+babel = Babel()
+scheduler = APScheduler()
 
 
 class Config:
     SCHEDULER_API_ENABLED = True
+    BABEL_DEFAULT_LOCALE = "fi"
 
 
-app = Flask(__name__)
-csrf = CSRFProtect(app)
-load_dotenv()
-app.config.from_object(Config())
-env = os.getenv("FLASK_ENV", "development")
-if env == "testing":
-    app.config["SECRET_KEY"] = os.getenv("TEST_SECRET_KEY")
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("TEST_DATABASE_URL")
-else:
-    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-app.config["BABEL_DEFAULT_LOCALE"] = "fi"
-app.config["DEBUG"] = os.getenv("FLASK_DEBUG", "0") == "1"
-
-babel = Babel(app)
-
-
+# korjaa
 def get_locale():
-    if "language" not in session:
+    from flask import has_request_context
+
+    if not has_request_context():
+        # if "language" not in session:
         return "fi"
-    return session.get("language", 0)
+    return session.get("language", "fi")
 
 
-babel.init_app(app, locale_selector=get_locale)
+def create_app(test_config=None):
+    load_dotenv()
 
-db = SQLAlchemy(app)
+    app = Flask(__name__)
+    app.config.from_object(Config())
 
-# initialize scheduler
-scheduler = APScheduler()
-scheduler.init_app(app)
-scheduler.start()
+    env = os.getenv("FLASK_ENV", "development")
 
-# make format_datestring accessible from jinja templates
-from src.tools.date_converter import format_datestring
+    if test_config is None:
+        if env == "testing":
+            app.config["SECRET_KEY"] = os.getenv("TEST_SECRET_KEY")
+            app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("TEST_DATABASE_URL")
+        else:
+            app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+            app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 
-app.jinja_env.globals.update(format_datestring=format_datestring)
+        app.config["DEBUG"] = os.getenv("FLASK_DEBUG", "0") == "1"
 
-from src import routes
+    else:
+        app.config.update(test_config)
+
+    csfr.init_app(app)
+    db.init_app(app)
+    babel.init_app(app, locale_selector=get_locale)
+
+    if test_config is None or env != "testing":
+        # initialize scheduler if not running tests
+        scheduler.init_app(app)
+        scheduler.start()
+
+    # make format_datestring accessible from jinja templates
+    app.jinja_env.globals.update(format_datestring=format_datestring)
+
+    from src.routes import bp
+
+    app.register_blueprint(bp)
+
+    return app
+
+
+app = create_app()
