4340ecf5af67aea045cd5737f938baa96a51ec23
Fialco <113672867+Fialco@users.noreply.github.com>
2025-10-10 09:44:48 +0300

    Start frontpage() route refactoring

--- src/repositories/survey_repository.py
+++ src/repositories/survey_repository.py
@@ -50,12 +50,12 @@ class SurveyRepository:
         """
         # Here we display all surveys created by a user that are not deleted, so in surveys table deleted=False
         try:
-            sql = "SELECT s.id FROM surveys s, survey_owners so WHERE (so.user_id=:user_id AND so.survey_id=s.id AND s.deleted=False)"
+            sql = "SELECT COUNT(s.id) FROM surveys s, survey_owners so WHERE (so.user_id=:user_id AND so.survey_id=s.id AND s.deleted=False)"
             result = db.session.execute(text(sql), {"user_id": user_id})
-            survey_list = result.fetchall()
-            if not survey_list:
+            count = result.fetchone()
+            if not count:
                 return False
-            return len(survey_list)
+            return count.count
         except Exception as e:  # pylint: disable=W0718
             print(e)
             return False
@@ -100,7 +100,29 @@ class SurveyRepository:
             user_id: The id of the user
         """
         try:
-            sql = "SELECT s.id, s.surveyname FROM surveys s, survey_owners so WHERE (so.user_id=:user_id AND closed=False AND s.id=so.survey_id AND s.deleted=False)"
+            sql = "SELECT s.id, s.surveyname, s.time_end FROM surveys s, survey_owners so WHERE (so.user_id=:user_id AND closed=False AND s.id=so.survey_id AND s.deleted=False)"
+            result = db.session.execute(text(sql), {"user_id": user_id})
+            surveys = result.fetchall()
+            if not surveys:
+                return False
+            return surveys
+        except Exception as e:  # pylint: disable=W0718
+            print(e)
+            return False
+
+    def get_active_surveys_and_response_count(self, user_id):
+        """
+        Get all active surveys and response counts to surveys where user is an owner.
+
+        args:
+            user_id: The id of the user
+        """
+        try:
+            sql = """SELECT s.id, s.surveyname, s.time_end, COUNT(us.user_id) AS response_count FROM surveys s 
+            JOIN survey_owners so ON s.id = so.survey_id 
+            LEFT JOIN user_survey_rankings us ON s.id = us.survey_id 
+            WHERE (so.user_id=1 AND closed=False AND s.id=so.survey_id AND s.deleted=False) 
+            GROUP BY s.id, s.surveyname"""
             result = db.session.execute(text(sql), {"user_id": user_id})
             surveys = result.fetchall()
             if not surveys:
