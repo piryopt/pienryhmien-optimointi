ac055fe809016ea8303550fbb2485b8399c789ad
Niilo Kuronen <niilo.kuronen@helsinki.fi>
2025-10-22 13:09:43 +0300

    fix frontpage changes

--- frontend/src/pages/FrontPage.jsx
+++ frontend/src/pages/FrontPage.jsx
@@ -1,7 +1,27 @@
+import { useTranslation } from "react-i18next";
+
 const FrontPage = () => {
+  const { t } = useTranslation();
+
   return (
     <div>
-      <h1>this is frontpage for now!</h1>
+      <div>
+        <a href="/surveys/create">
+          {t(
+            "Luo uusi kysely tai tuo valmiit vastausvaihtoehdot csv-tiedostosta",
+          )}
+        </a>
+      </div>
+      <div>
+        <a href="/multiphase/survey/create">
+          {t(
+            "Luo uusi monivaiheinen kysely, jossa määritetään eri vaiheiden vastausvaihtoehdot",
+          )}
+        </a>
+      </div>
+      <div>
+        <a href="/surveys">{t("Näytä vanhat kyselyt")}</a>
+      </div>
     </div>
   );
 };
