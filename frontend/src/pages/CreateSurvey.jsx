80531234d80e58aa84144c73ac0e9bba909728aa
Joni Ursin <joni.ursin@gmail.com>
2025-10-29 22:12:51 +0200

    add CreateSurvey page

---
+++ frontend/src/pages/CreateSurvey.jsx
@@ -0,0 +1,16 @@
+import { useTranslation } from "react-i18next";
+import SurveyForm from "../components/create_survey_components/SurveyForm";
+import "../static/css/create_survey.css";
+
+const CreateSurvey = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div>
+      <h1>{t("Luo uusi kysely")}</h1>
+      <SurveyForm />
+    </div>
+  );
+};
+
+export default CreateSurvey;
