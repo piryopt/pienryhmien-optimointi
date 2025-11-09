ed4b3dfd76c6e8d6ee2c1463c6cb08d8dcd0c40a
Fialco <113672867+Fialco@users.noreply.github.com>
2025-11-09 13:08:56 +0200

    Update translations
    - Add file for dynamic translations so they are not removed during
      i18next-cli extract

---
+++ frontend/src/i18n-dynamic-keys.js
@@ -0,0 +1,10 @@
+t("Luo uusi kysely");
+t("Luotuja kyselyitä");
+t("Käynnissä olevat kyselyt");
+t("Luo uusi kysely tai tuo valmiit vastausvaihtoehdot csv-tiedostosta");
+t(
+  "Luo uusi monivaiheinen kysely, jossa määritetään eri vaiheiden vastausvaihtoehdot"
+);
+t("Näytä vanhat kyselyt");
+t("Vastaukset");
+t("etusivu.Vastausaika päättyy");
