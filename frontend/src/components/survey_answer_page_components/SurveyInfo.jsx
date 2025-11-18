19a5f7cff00e71a018d55ce212bfc6e500dcb1e0
BorisBanchev <boris.banchev03@gmail.com>
2025-11-19 01:41:56 +0200

    Modify survey answer page survey info to match min choices and a_d_c

--- frontend/src/components/survey_answer_page_components/SurveyInfo.jsx
+++ frontend/src/components/survey_answer_page_components/SurveyInfo.jsx
@@ -1,17 +1,65 @@
-const SurveyInfo = ({ survey, additionalInfo }) => (
-  <>
-    <p className="deadline">Vastausaika päättyy {survey.deadline}</p>
-    <p style={{ padding: "1.5em 0" }}>{survey.description}</p>
-    <p className="instructions">
-      <i>
-        Raahaa oikean reunan listasta vähintään {survey.min_choices} vaihtoehtoa
-        <span className="highlight"> vihreään</span> laatikkoon.
-      </i>
-      {additionalInfo ? (
-        <i> Klikkaa valintavaihtoehtoa nähdäksesi siitä lisätietoa.</i>
-      ) : null}
-    </p>
-  </>
-);
+const SurveyInfo = ({ survey, additionalInfo, choices }) => {
+  const min_choices = Number(survey.min_choices);
+  const a_d_c = Number(survey.denied_allowed_choices);
+  const total = choices.length;
+
+  const isAll = total > 0 && min_choices === total;
+  const denySome = a_d_c > 0;
+
+  return (
+    <>
+      <p className="deadline">Vastausaika päättyy {survey.deadline}</p>
+      <p style={{ padding: "1.5em 0" }}>{survey.description}</p>
+
+      <p className="instructions">
+        <i>
+          {isAll && !denySome && (
+            <>
+              Raahaa kaikki vaihtoehdot{" "}
+              <span className="highlight-green">vihreään</span> laatikkoon.
+            </>
+          )}
+
+          {isAll && denySome && (
+            <>
+              Raahaa kaikki vaihtoehdot{" "}
+              <span className="highlight-green">vihreään</span> tai{" "}
+              <span className="highlight-red">punaiseen</span> laatikkoon.
+              <br />
+              <span>
+                Voit kieltää {a_d_c} vaihtoehtoa{" "}
+                <span className="highlight-red">punaiseen</span> laatikkoon.
+              </span>
+            </>
+          )}
+
+          {!isAll && !denySome && (
+            <>
+              Raahaa ainakin {min_choices} vaihtoehtoa{" "}
+              <span className="highlight-green">vihreään</span> laatikkoon.
+            </>
+          )}
+
+          {!isAll && denySome && (
+            <>
+              Raahaa ainakin {min_choices} vaihtoehtoa{" "}
+              <span className="highlight-green">vihreään</span> tai{" "}
+              <span className="highlight-red">punaiseen</span> laatikkoon.
+              <br />
+              <span>
+                Voit kieltää {a_d_c} vaihtoehtoa{" "}
+                <span className="highlight-red">punaiseen</span> laatikkoon.
+              </span>
+            </>
+          )}
+        </i>
+
+        {additionalInfo ? (
+          <i> Klikkaa valintavaihtoehtoa nähdäksesi siitä lisätietoa.</i>
+        ) : null}
+      </p>
+    </>
+  );
+};
 
 export default SurveyInfo;
