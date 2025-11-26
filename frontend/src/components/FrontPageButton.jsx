ccd398d3cf9dc9229da5c8cfa767886522102068
Fialco <113672867+Fialco@users.noreply.github.com>
2025-11-26 16:35:09 +0200

    Refactor trash and frontpage components

---
+++ frontend/src/components/FrontPageButton.jsx
@@ -0,0 +1,44 @@
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+
+const FrontPageButton = ({
+  path,
+  imgSrc,
+  mainText,
+  additionalText,
+  topRightText,
+  additionalVars
+}) => {
+  const { t } = useTranslation();
+
+  return (
+    <Link
+      to={path}
+      className="list-group-item list-group-item-action"
+      style={{ borderRadius: "12px" }}
+    >
+      <div className="d-flex w-100">
+        <div className="d-flex w-100">
+          <img
+            src={imgSrc}
+            alt=""
+            width="34"
+            height="30"
+            className=""
+            style={{ marginRight: "8px" }}
+          />
+          <p style={{ fontSize: "130%" }}>{t(mainText, additionalVars)}</p>
+        </div>
+
+        {topRightText && (
+          <small className="text-muted text-nowrap">
+            {t(topRightText, additionalVars)}
+          </small>
+        )}
+      </div>
+      <small className="text-muted">{t(additionalText, additionalVars)}</small>
+    </Link>
+  );
+};
+
+export default FrontPageButton;
