fa1ae8e824a67c59146d1833bbdeda5684b1e239
kuroniil <niilo.kuronen@helsinki.fi>
2025-12-04 16:37:18 +0200

    Small UI improvement for smaller screen sizes

--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -6,26 +6,28 @@ const Table = ({
   cellSpacing = 10
 }) => {
   return (
-    <table cellSpacing={cellSpacing} className={className}>
-      <thead className="table-dark">
-        <tr>
-          {columns.map((col, i) => (
-            <th key={i} style={col.style || null}>
-              <p>
-                <img
-                  src={col.icon}
-                  width={24}
-                  height={24}
-                  className="d-inline-block align-text-top"
-                />
-                &nbsp;{col.title}
-              </p>
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>{data.map((item, i) => renderRow(item, i))}</tbody>
-    </table>
+    <div style={{ overflowX: "auto" }}>
+      <table cellSpacing={cellSpacing} className={className}>
+        <thead className="table-dark">
+          <tr>
+            {columns.map((col, i) => (
+              <th key={i} style={col.style || null}>
+                <p>
+                  <img
+                    src={col.icon}
+                    width={24}
+                    height={24}
+                    className="d-inline-block align-text-top"
+                  />
+                  &nbsp;{col.title}
+                </p>
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>{data.map((item, i) => renderRow(item, i))}</tbody>
+      </table>
+    </div>
   );
 };
 
